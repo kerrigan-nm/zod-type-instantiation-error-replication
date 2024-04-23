477577ab01058a6f2051b5957ad5981e833aacc0
Kerrigan Joseph <kerriganjoseph@northwesternmutual.com>
2024-04-23 10:18:31 -0500

    feat: add reproduction between 3.23.0 and 3.23.3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { z as z_3_23_0 } from 'zod-3.23.0'
 import { z as z_3_23_1 } from 'zod-3.23.1'
+import { z as z_3_23_3 } from 'zod-3.23.3'
 
 z_3_23_1.object({
+  str: z_3_23_0.string()
+})
+
+z_3_23_3.object({
   str: z_3_23_0.string()
 })
